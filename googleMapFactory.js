c3a57d3399a93a0f4fcfb11fc13f0a07d20b7505
Allen H. Thompson <allenhthompson1@gmail.com>
2016-09-21 17:41:13 -0400

    Revert "Removed googleMapFactory module."
    This reverts commit f9183f5d7af387ce4746c2eb8609ddb5da617894.
    
    # Conflicts:
    #	weathermap.js

--- googleMapFactory.js
+++ googleMapFactory.js
@@ -1,82 +1,6 @@
-var cityIds = [
-     4180439,
-     5128638,
-     4560349,
-     4726206,
-     4671654,
-     5809844,
-     5368361,
-     5391811,
-     5308655,
-     4684888,
-     4887398,
-     5391959,
-     5392171,
-     4164138,
-     4273837,
-     5746545,
-     4699066,
-     5419384,
-     4990729
-];
-var app = angular.module('my-app', ['ngRoute', 'googleMapFactory']);
-
-app.config(function($routeProvider) {
-     $routeProvider
-     .when('/', {
-          controller: 'WeatherMapController',
-          templateUrl: 'overview.html'
-     })
-     .when('/city/:cityId', {
-          controller: 'ForecastController',
-          templateUrl: 'forecast.html'
-     });
-});
-
-app.controller('ForecastController', function($scope, googleMap, $routeParams, weatherService) {
-     var cityId = $routeParams.cityId;
-     weatherService.getForecastForCity(cityId, function(weatherMapData){
-
-          var results = weatherMapData.list;
-          $scope.results = results;
-          // var map = googleMap.newMap(39.99727, -94.578567, 10);
-          // googleMap.plotData(results, map);
-     });
-});
-
-app.factory('weatherService', function($http){
-     var someMethod = function(cityIds, callback) {
-          $http({
-               url: 'http://api.openweathermap.org/data/2.5/group',
-               params: {
-                    id: cityIds.join(','),
-                    units: 'imperial',
-                    APPID: APPID
-               }
-          }).success(function(data) {
-               callback(data);
-          });
-     };
-     var someOtherMethod = function(cityId, callback) {
-          $http({
-               url: 'http://api.openweathermap.org/data/2.5/forecast',
-               params: {
-                    id: cityId,
-                    units: 'imperial',
-                    APPID: APPID
-               }
-          }).success(callback);
-     };
-     var APPID = 'e9348133b64a0819b26b734ad2ec4a19';
-     return {
-          getWeatherByCityIds: someMethod,
-          getForecastForCity: someOtherMethod
-     };
-});
-//put google map code
-
-<<<<<<< HEAD
-app.factory('googleMap', function() {
+googleMapModule.module('googleMapModule', [])
+.factory('googleMap', function() {     
+//app.factory('googleMap', function() {
      return {
           newMap: function(lat, long, zoom) {
                var mapOptions = {
@@ -101,6 +25,10 @@ app.factory('googleMap', function() {
                var marker = markerDictionary[results.id];
                infoWindow.setContent(contentString);
                infoWindow.open(map, marker);
+               marker.addListener('click', function() {
+                 infoWindow.close();
+               });
+               console.log(infoWindow)
           },
           plotData: function(results, map) {
                var service = this;
@@ -131,24 +59,4 @@ app.factory('googleMap', function() {
                });
           }
      };
-});
-=======
-
->>>>>>> parent of f9183f5... Removed googleMapFactory module.
-
-var infoWindows = [];
-var markerDictionary = {};
-
-app.controller('WeatherMapController', function($scope, $http, weatherService, googleMap){
-
-     $scope.openInfoWindow = function(result) {
-          googleMap.openInfoWindow(result);
-     };
-
-     weatherService.getWeatherByCityIds(cityIds, function(weatherMapData){
-          var results = weatherMapData.list;
-          $scope.results = results;
-          var map = googleMap.newMap(39.99727, -94.578567, 4);
-          googleMap.plotData(results, map);
-     });
 });
